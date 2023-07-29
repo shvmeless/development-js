99f5f24b754a86835469df16cd08607c805359fa
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2023-07-28 21:20:06 -0400

    feat(record): add `filter` method

--- src/utils/record-iterator-async.ts
+++ src/utils/record-iterator-async.ts
@@ -10,3 +10,18 @@ export async function forEach <K extends string, V> (object: Record<K, V>, callb
     await callback(value, key)
   }
 }
+
+/**
+ * Returns a copy of the given object with only the properties that pass the validation function.
+ * @param object The object to filter.
+ * @param callback The function that validates each property.
+ * @returns A copy of the given object with only the properties that pass the validation function.
+*/
+export async function filter <K extends string, V> (object: Record<K, V>, callback: (value: V, key: K) => Promise<boolean>): Promise<Record<K, V>> {
+  const result: Record<string, V> = {}
+  for (const [key, value] of entries(object)) {
+    const condition = await callback(value, key)
+    if (condition) result[key] = value
+  }
+  return result as Record<K, V>
+}
--- src/utils/record-iterator.ts
+++ src/utils/record-iterator.ts
@@ -10,3 +10,18 @@ export function forEach <K extends string, V> (object: Record<K, V>, callback: (
     callback(value, key)
   }
 }
+
+/**
+ * Returns a copy of the given object with only the properties that pass the validation function.
+ * @param object The object to filter.
+ * @param callback The function that validates each property.
+ * @returns A copy of the given object with only the properties that pass the validation function.
+*/
+export function filter <K extends string, V> (object: Record<K, V>, callback: (value: V, key: K) => boolean): Record<K, V> {
+  const result: Record<string, V> = {}
+  for (const [key, value] of entries(object)) {
+    const condition = callback(value, key)
+    if (condition) result[key] = value
+  }
+  return result as Record<K, V>
+}
