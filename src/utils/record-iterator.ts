4cf5f64c1912be4a1031525c0db4033b45445005
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2023-07-28 21:20:32 -0400

    feat(record): add `map` method

--- src/utils/record-iterator.ts
+++ src/utils/record-iterator.ts
@@ -25,3 +25,20 @@ export function filter <K extends string, V> (object: Record<K, V>, callback: (v
   }
   return result as Record<K, V>
 }
+
+/**
+ * Maps each property of the given object.
+ * If the callback returns undefined, the property will be omitted.
+ * @param object The object to map.
+ * @param callback The function to call for each property.
+ * @returns A copy of the given object with the mapped properties.
+*/
+export function map <K extends string, V, N> (object: Record<K, V>, callback: (value: V, key: K) => N | undefined): Record<K, N> {
+  const result: Record<string, N> = {}
+  for (const [key, value] of entries(object)) {
+    const item = callback(value, key)
+    if (item === undefined) continue
+    result[key] = item
+  }
+  return result as Record<K, N>
+}
