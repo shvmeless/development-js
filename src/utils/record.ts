31227d4108e2efae4548fd1c2e8cae929f7ded01
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2023-07-28 21:17:12 -0400

    feat(record): add `keys` method

--- src/utils/record.ts
+++ src/utils/record.ts
@@ -6,3 +6,12 @@
 export function entries <K extends string, V> (object: Record<K, V>): [K, V][] {
   return Object.entries(object) as [K, V][]
 }
+
+/**
+ * Returns the keys of the given object.
+ * @param object The object to get the keys of.
+ * @returns The keys of the given object.
+*/
+export function keys <K extends string> (object: Record<K, unknown>): K[] {
+  return Object.keys(object) as K[]
+}
